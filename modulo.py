560a91542eabd0da19872ff83c2e6d04781f1067
IuryDiasCoelho <iurydiascoelho31@hotmail.com>
2026-03-04 00:35:58 -0300

    alterações nas funcoes, finalizando projeto

--- modulo.py
+++ modulo.py
@@ -1,3 +1,6 @@
+estoque={}
+financeiro={}
+caixa=0
 print("Bem vindo ao seu gestor de estoque")
 
 def menu():
@@ -9,45 +12,47 @@ def menu():
     print("5- Valor em caixa")
     print("6- Sair\n")
 
+def estoque_disponivel():
+    print("\nBuscando estoque disponivel...")
+    for produto, qtd in estoque.items():
+        valor = financeiro[produto]
+        print(f"{produto} - {qtd} unidades - R${valor}\n")
+
 def cadastrar():
-    protudo = input('digite o nome do produto: ')
+    produto = input('digite o nome do produto: ')
     quantidade = int(input('digite a quantidade: '))
     valor = (input('digite o valor: '))
     valor_limpo = float(valor.replace(',', '.'))
-    estoque[protudo] = quantidade
-    financeiro[protudo] = valor_limpo
+    estoque[produto] = quantidade
+    financeiro[produto] = valor_limpo
 
 def venda():
+    global caixa
+    estoque_disponivel()
     venda_produto = input('digite o nome do produto: ')
-
-    if venda in estoque:
+    if venda_produto in estoque:
         try:
             print(estoque[venda_produto], "unidades")
             quantidade = int(input('digite a quantidade a ser vendida: '))
-
             if quantidade <= 0:
                 print("A quantidade deve ser maior que zero.")
             elif quantidade > estoque[venda_produto]:
                 print("Quantidade maior do que o estoque disponível.")
             else:
                 estoque[venda_produto] -= quantidade
+                soma = financeiro[venda_produto] * quantidade
+                caixa += soma
+
                 print("vendendo ", quantidade, " unidades de ", venda_produto)
                 print("registrando venda...")
-                print("Estoque Atualizado\n", list(estoque.items()))
-                return quantidade , venda_produto
+                print("Estoque Atualizado")
+                estoque_disponivel()
         except ValueError:
             print("Digite uma quantidade válida (número inteiro).")
     else:
         print("Produto não encontrado no estoque.")
 
 
-def valor_em_caixa(venda_produto, quantidade):
-    soma=financeiro[venda_produto]*quantidade
-    print(soma)
-    print("valor em caixa...")
-
-estoque={}
-financeiro={}
 while True:
     menu()
     usuario=input('digite a ação que quer realizar: ').lower()
@@ -57,29 +62,25 @@ while True:
             print("cadastrando produto...")
 
         case "atualizar estoque" | "2":
-            print(list(estoque.items()))
-            print(list(financeiro.items()))
+            estoque_disponivel()
             cadastrar()
-            print(list(estoque.items()))
-            print(list(financeiro.items()))
+            estoque_disponivel()
             print("atualizando estoque...")
 
         case "registrar venda" | "3":
+
             venda()
 
         case "estoque disponivel" | "4":
-            print("Buscando estoque disponivel...")
-            print(estoque)
-            print(financeiro)
+            estoque_disponivel()
 
         case "valor em caixa" | "5":
-
-
-            print("valor em caixa...")
+            print("valor em caixa: ", caixa, "R$")
 
         case "sair" | "6":
             print("saindo...")
             break
+
         case _:
             print("\nDigite novamente o comando")
 
